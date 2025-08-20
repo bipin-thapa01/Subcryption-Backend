3291abe483366c41b9325e25ffd4dbfb3376bf0b
Bipin Thapa <yukichin638@gmail.com>
2025-08-20 09:11:11 +0545

    Environment Variable Created

--- database.js
+++ database.js
@@ -1,8 +1,7 @@
+require('dotenv').config();
+
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = 'https://ttocbvxrbkschnfevopk.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0b2NidnhyYmtzY2huZmV2b3BrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU1NzQwMzQsImV4cCI6MjA3MTE1MDAzNH0.-x4xWT9YXkKhkBQe9r0wRq29rDvzS54nwr1wy8lVF6I';
-
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabase = createClient(process.env.supabaseUrl, process.env.supabaseKey);
 
 module.exports = {supabase};
